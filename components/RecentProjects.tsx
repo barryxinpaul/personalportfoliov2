a7e98032ed2e0dd574407212c0603206eff7481c
barryxinpaul <bpaul33@uwo.ca>
2024-11-22 21:04:50 -0500

    Update Projects to allow link

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -18,6 +18,7 @@ const RecentProjects = () => {
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
           >
+            <a href={item.link} target="_blank">
             <PinContainer
               title={item.link}
               href={item.link}
@@ -73,6 +74,7 @@ const RecentProjects = () => {
                 </div>
               </div>
             </PinContainer>
+            </a>
           </div>
         ))}
       </div>
